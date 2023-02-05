a5f543756cb66ee36eb79d38e7b62caf174fe586
LuckyMan612 <75744132+LuckyMan612@users.noreply.github.com>
2023-02-05 21:50:54 +0100

    Update script.js

--- myGames/script.js
+++ myGames/script.js
@@ -1,36 +1,88 @@
-const gameContainer = document.getElementById("gameContainer");
+const games = [
+  {
+    title: "Escape The Dungeon",
+    description: "You are escaping from the dungeons!",
+    date: "24.01.2023",
+    link: "https://lucky2337.itch.io/escape-dungeon"
+  },
+  {
+    title: "Escape The Dungeon 2",
+    description: "You are escaping from the dungeons! But version 2 :D",
+    date: "24.01.2023",
+    link: "https://lucky2337.itch.io/escape-the-dungeon-2"
+  },
+  {
+    title: "1 minute game",
+    description: "This game was created in a minute!",
+    date: "09.01.2023",
+    link: "https://lucky2337.itch.io/1-minute-game"
+  },
+  {
+    title: "Reaction Game",
+    description: "A game in which you test your reaction time.",
+    date: "04.01.2023",
+    link: "https://lucky2337.itch.io/reaction-game"
+  }
+];
 
-const gameJam = document.getElementById("gameJam");
-const game1 = gameJam.querySelector(".game");
-game1.querySelector(".game-title").innerHTML = "Escape The Dungeon";
-game1.querySelector(".game-description").innerHTML = "You are escaping from the dungeons!";
-game1.querySelector(".game-date").innerHTML = "24.01.2023";
-game1.querySelector(".game-button").onclick = function() {
-  window.location.href = "https://lucky2337.itch.io/escape-dungeon";
-};
+const gamesContainer = document.querySelector("#games-container");
 
-// Tutaj możesz dodać kolejne gry w kacji Game Jam
-const game2 = gameJam.querySelector(".game2");
-game2.querySelector(".game-title").innerHTML = "Escape The Dungeon 2";
-game2.querySelector(".game-description").innerHTML = "You are escaping from the dungeons! But version 2 :D";
-game2.querySelector(".game-date").innerHTML = "24.01.2023";
-game2.querySelector(".game-button").onclick = function() {
-  window.location.href = "https://lucky2337.itch.io/escape-the-dungeon-2";
-};
+for (let i = 0; i < games.length; i++) {
+  const game = games[i];
 
-// Tutaj możesz dodać kolejne kategorie gier i gry w każdej z nich
-const game3 = gameJam.querySelector(".game3");
-game2.querySelector(".game-title").innerHTML = "1 minute game";
-game2.querySelector(".game-description").innerHTML = "This game was created in a minute!";
-game2.querySelector(".game-date").innerHTML = "09.01.2023";
-game2.querySelector(".game-button").onclick = function() {
-  window.location.href = "https://lucky2337.itch.io/1-minute-game";
-};
-const game4 = gameJam.querySelector(".game4");
-game2.querySelector(".game-title").innerHTML = "Reaction Game";
-game2.querySelector(".game-description").innerHTML = "A game in which you test your reaction time.";
-game2.querySelector(".game-date").innerHTML = "04.01.2023";
-game2.querySelector(".game-button").onclick = function() {
-  window.location.href = "https://lucky2337.itch.io/reaction-game";
-};
+  const gameElement = document.createElement("div");
+ gameElement.innerHTML = <h2>${game.title}</h2> <p>${game.description}</p> <p>${game.date}</p> <a href="${game.link}" target="_blank">Play now</a> ;
 
+gamesContainer.appendChild(gameElement);
+}
+
+// Kategorie gier
+
+const categories = [
+{
+name: "Game Jam",
+games: [
+games[0],
+games[2]
+]
+},
+{
+name: "Escape Games",
+games: [
+games[0],
+games[1]
+]
+},
+{
+name: "Quick Games",
+games: [
+games[2]
+]
+}
+];
+
+const categoriesContainer = document.querySelector("#categories-container");
+
+for (let i = 0; i < categories.length; i++) {
+const category = categories[i];
+
+const categoryElement = document.createElement("div");
+categoryElement.innerHTML = <h2>${category.name}</h2>;
+
+const gamesList = document.createElement("ul");
+for (let j = 0; j < category.games.length; j++) {
+const game = category.games[j];
+const gameItem = document.createElement("li");
+gameItem.innerHTML = `
+  <h3>${game.title}</h3>
+  <p>${game.description}</p>
+  <p>${game.date}</p>
+  <a href="${game.link}" target="_blank">Play now</a>
+`;
+
+gamesList.appendChild(gameItem);
+}
+
+categoryElement.appendChild(gamesList);
+categoriesContainer.appendChild(categoryElement);
+}
