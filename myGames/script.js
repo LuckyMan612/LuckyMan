824f3b809728a5d1977f4f3e901ec5a196ee6d84
LuckyMan612 <75744132+LuckyMan612@users.noreply.github.com>
2023-02-05 21:22:59 +0100

    Update script.js

--- myGames/script.js
+++ myGames/script.js
@@ -10,30 +10,20 @@ game1.querySelector(".game-button").onclick = function() {
 };
 
 // Tutaj możesz dodać kolejne gry w kacji Game Jam
+const game2 = gameJam.querySelector(".game2");
+game2.querySelector(".game-title").innerHTML = "Escape The Dungeon 2";
+game2.querySelector(".game-description").innerHTML = "You are escaping from the dungeons! But version 2 :D";
+game2.querySelector(".game-date").innerHTML = "24.01.2023";
+game2.querySelector(".game-button").onclick = function() {
+  window.location.href = "https://lucky2337.itch.io/escape-the-dungeon-2";
+};
 
 // Tutaj możesz dodać kolejne kategorie gier i gry w każdej z nich
-
-// Tworzenie nowej kategorii
-const newCategory = document.createElement("div");
-newCategory.classList.add("game-category");
-newCategory.innerHTML = `
-
-  <h2>Nowa Kategoria</h2>
-  <div class="game">
-    <h3 class="game-title">Nazwa gry</h3>
-    <p class="game-description">Opis gry</p>
-    <p class="game-date">Data gry</p>
-    <button class="game-button">Przejdź do gry</button>
-  </div>
-`;
-// Dodawanie nowej kategorii do kontenera gier
-gameContainer.appendChild(newCategory);
-
-// Modyfikacja nowej kategorii
-const gameInNewCategory = newCategory.querySelector(".game");
-gameInNewCategory.querySelector(".game-title").innerHTML = "Nazwa gry w nowej kategorii";
-gameInNewCategory.querySelector(".game-description").innerHTML = "Opis gry w nowej kategorii";
-gameInNewCategory.querySelector(".game-date").innerHTML = "Data gry w nowej kategorii";
-gameInNewCategory.querySelector(".game-button").onclick = function() {
-window.location.href = "link_do_strony_gry_w_nowej_kategorii";
+const game2 = gameJam.querySelector(".game");
+game2.querySelector(".game-title").innerHTML = "Escape The Dungeon 2";
+game2.querySelector(".game-description").innerHTML = "You are escaping from the dungeons! But version 2 :D";
+game2.querySelector(".game-date").innerHTML = "24.01.2023";
+game2.querySelector(".game-button").onclick = function() {
+  window.location.href = "https://lucky2337.itch.io/escape-the-dungeon-2";
 };
+
