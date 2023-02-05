624f0cb548e32c84b7bdb437e6444a0429cd232c
LuckyMan612 <75744132+LuckyMan612@users.noreply.github.com>
2023-02-05 21:30:46 +0100

    Update script.js

--- myGames/script.js
+++ myGames/script.js
@@ -19,11 +19,18 @@ game2.querySelector(".game-button").onclick = function() {
 };
 
 // Tutaj możesz dodać kolejne kategorie gier i gry w każdej z nich
-const game2 = gameJam.querySelector(".game");
-game2.querySelector(".game-title").innerHTML = "Escape The Dungeon 2";
-game2.querySelector(".game-description").innerHTML = "You are escaping from the dungeons! But version 2 :D";
-game2.querySelector(".game-date").innerHTML = "24.01.2023";
+const game3 = gameJam.querySelector(".game3");
+game2.querySelector(".game-title").innerHTML = "1 minute game";
+game2.querySelector(".game-description").innerHTML = "This game was created in a minute!";
+game2.querySelector(".game-date").innerHTML = "09.01.2023";
 game2.querySelector(".game-button").onclick = function() {
-  window.location.href = "https://lucky2337.itch.io/escape-the-dungeon-2";
+  window.location.href = "https://lucky2337.itch.io/1-minute-game";
+};
+const game4 = gameJam.querySelector(".game4");
+game2.querySelector(".game-title").innerHTML = "Reaction Game";
+game2.querySelector(".game-description").innerHTML = "A game in which you test your reaction time.";
+game2.querySelector(".game-date").innerHTML = "04.01.2023";
+game2.querySelector(".game-button").onclick = function() {
+  window.location.href = "https://lucky2337.itch.io/reaction-game";
 };
 
